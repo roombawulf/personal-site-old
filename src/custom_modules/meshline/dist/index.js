bd77cf7588c080bdeb10bf3d72721e034e96370a
wulfgar-97 <prydax997@gmail.com>
2023-02-07 14:42:21 +0000

    fix import body to top

--- src/custom_modules/meshline/dist/index.js
+++ src/custom_modules/meshline/dist/index.js
@@ -1,10 +1,10 @@
+import * as THREE from "three";
 var __defProp = Object.defineProperty;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __publicField = (obj, key, value) => {
   __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
   return value;
 };
-import * as THREE from "three";
 function memcpy(src, srcOffset, dst, dstOffset, length) {
   let i;
   src = src.subarray || src.slice ? src : src.buffer;

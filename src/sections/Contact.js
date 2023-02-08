64103b13349f2c130a86fac54177f617e7cd7031
wulfgar-97 <prydax997@gmail.com>
2023-02-08 23:50:00 +0000

    under construction

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -4,7 +4,7 @@ export default function Contact(){
     return (
         <AnimatePage>
             <section className='contact'>
-                this is a contacts section.
+                ...under construction...
             </section>
         </AnimatePage>
     )

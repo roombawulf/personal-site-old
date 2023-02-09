bdadc145a47324725227b37da8c56fe886516336
wulfgar-97 <prydax997@gmail.com>
2023-02-09 16:40:08 +0000

    temporary contact edit

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -4,7 +4,14 @@ export default function Contact(){
     return (
         <AnimatePage>
             <section className='contact'>
-                ...under construction...
+                <h2>E-mail</h2>
+                <h4>
+                    har1sraza@hotmail.co.uk
+                </h4>
+                <h2>LinkedIn</h2>
+                <h4>
+                    https://www.linkedin.com/in/haris-raza-9b1270199/
+                </h4>
             </section>
         </AnimatePage>
     )
